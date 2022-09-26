bac530adc682cd217f25e625f560f641fc03b099
eldreco <andreffcd@gmail.com>
2022-09-26 22:22:30 +0100

    version 3.5 - test new look
    Updated to URP(Universal Render Pipeline) and testing a new look with a darker style including glowing objects

--- Assets/Scripts/FloorController.cs
+++ Assets/Scripts/FloorController.cs
@@ -8,16 +8,16 @@ public class FloorController : MonoBehaviour
     public GameObject floorMiddle;
     public GameObject floorRight;
 
-    private Animator fLeftAnim;
-    private Animator fMiddleAnim;
-    private Animator fRightAnim;
-
     public GameObject player;
 
+    private Renderer middleRenderer;
+    private Renderer leftRenderer;
+    private Renderer rightRenderer;
+
     private void Start() {
-        fLeftAnim = floorLeft.GetComponent<Animator>();
-        fMiddleAnim = floorMiddle.GetComponent<Animator>();
-        fRightAnim = floorRight.GetComponent<Animator>();
+        middleRenderer = floorMiddle.GetComponent<Renderer>();
+        leftRenderer = floorLeft.GetComponent<Renderer>();
+        rightRenderer = floorRight.GetComponent<Renderer>();
     }
 
     private void Update() {
@@ -26,17 +26,17 @@ public class FloorController : MonoBehaviour
 
     private void PlayerPosCheck(){
         if(player.GetComponent<PlayerController>().getPos() == 1){
-            fLeftAnim.SetBool("IsOn" , false);
-            fMiddleAnim.SetBool("IsOn" , true);
-            fRightAnim.SetBool("IsOn" , false);
+            leftRenderer.material.color = new Color(0.15f, 0.15f, 0.15f);
+            middleRenderer.material.color = Color.white;
+            rightRenderer.material.color = new Color(0.15f, 0.15f, 0.15f);
         } else if(player.GetComponent<PlayerController>().getPos() == 0){
-            fLeftAnim.SetBool("IsOn" , true);
-            fMiddleAnim.SetBool("IsOn" , false);
-            fRightAnim.SetBool("IsOn" , false);
+            leftRenderer.material.color = Color.white;
+            middleRenderer.material.color = new Color(0.15f, 0.15f, 0.15f);
+            rightRenderer.material.color = new Color(0.15f, 0.15f, 0.15f);
         } else if(player.GetComponent<PlayerController>().getPos() == 2){
-            fLeftAnim.SetBool("IsOn" , false);
-            fMiddleAnim.SetBool("IsOn" , false);
-            fRightAnim.SetBool("IsOn" , true);
+            leftRenderer.material.color = new Color(0.15f, 0.15f, 0.15f);
+            middleRenderer.material.color = new Color(0.15f, 0.15f, 0.15f);
+            rightRenderer.material.color = Color.white;
         }
     }
 
