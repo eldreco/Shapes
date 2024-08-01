ef6a4e3cc00640b86d420ab10ebe7fec23771f86
André Florentino Dias <andreffcd@gmail.com>
2024-08-01 22:42:41 +0200

    Renamed variables and fixed trigger bug

--- Assets/Scripts/Shapes/ShapesManager.cs
+++ Assets/Scripts/Shapes/ShapesManager.cs
@@ -5,11 +5,11 @@ public class ShapesManager : MonoBehaviour
 {
     public static ShapesManager Instance;
 
-    public enum Shape{Cylinder, Cube, Pyramid};
+    public enum Shape{Cylinder, Hexagon, Pyramid};
     public Shape ActiveShape {get; private set;}
     public Dictionary<Shape, Mesh> ShapeMeshMap {get; private set;} = new();
 
-    [SerializeField] private Mesh cubeMesh;
+    [SerializeField] private Mesh hexagonMesh;
     [SerializeField] private Mesh pyramidMesh;
     [SerializeField] private Mesh cylinderMesh;
 
@@ -19,10 +19,10 @@ public class ShapesManager : MonoBehaviour
     }
 
     private void Start() {
-        ShapeMeshMap.Add(Shape.Cube, cubeMesh);
+        ShapeMeshMap.Add(Shape.Hexagon, hexagonMesh);
         ShapeMeshMap.Add(Shape.Pyramid, pyramidMesh);
         ShapeMeshMap.Add(Shape.Cylinder, cylinderMesh);
-        SetActiveShape(Shape.Cube);
+        SetActiveShape(Shape.Hexagon);
         GameManager.Instance.ActiveGameMode = GameMode.Shapes;
     }
 
@@ -35,9 +35,9 @@ public class ShapesManager : MonoBehaviour
         ShapesPlayerController.Instance.ChangeShape(Shape.Cylinder);
     }
 
-    public void PressSquare(){
-        SetActiveShape(Shape.Cube);
-        ShapesPlayerController.Instance.ChangeShape(Shape.Cube);
+    public void PressHexagon(){
+        SetActiveShape(Shape.Hexagon);
+        ShapesPlayerController.Instance.ChangeShape(Shape.Hexagon);
     }
 
     public void PressTriangle(){
